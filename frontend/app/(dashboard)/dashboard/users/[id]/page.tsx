493612c7a438b1fa620be50978f4eb276768d0c9
jyablonski9 <jyablonski9@gmail.com>
2025-04-24 17:32:17 -0700

    yeee

--- frontend/app/(dashboard)/dashboard/users/[id]/page.tsx
+++ frontend/app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function page({ params }: { params: { id: string } }) {
-  const { id } = params;
+// If you're not using `await` on `params`, just destructure and use it directly
+export default async function Page({ params }: {
+  params: Promise<{
+    id: string
+  }>
+}) {
+  // You do NOT need `await` on `params`, because it's already resolved
+  const { id } = await params;
 
   return <div>User Profile: {id}</div>;
 }
-
-export default page;
