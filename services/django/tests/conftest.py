6c412f631a8a76b25b2ded433de7e9e122d7218b
jyablonski9 <jyablonski9@gmail.com>
2025-12-22 07:46:46 -0800

    z

--- services/django/tests/conftest.py
+++ services/django/tests/conftest.py
@@ -4,31 +4,17 @@ import pytest
 from django.db import connection
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lotus_admin.settings")
+# Disable AdminOnlyMiddleware in tests
+# TODO: Fix this once core models can be managed=True
+os.environ.setdefault("DJANGO_TEST", "1")
 
 
 @pytest.fixture(scope="session", autouse=True)
-def setup_test_database(django_db_createdb, django_db_blocker):
+def setup_test_database(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         with connection.cursor() as cursor:
             cursor.execute("CREATE SCHEMA IF NOT EXISTS source")
             cursor.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-            cursor.execute("SET search_path TO source, public")
-            cursor.execute("DROP TABLE IF EXISTS source.users CASCADE")
-            cursor.execute("""
-                CREATE TABLE source.users (
-                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                    email VARCHAR NOT NULL UNIQUE,
-                    password VARCHAR,
-                    salt VARCHAR,
-                    oauth_provider VARCHAR,
-                    role VARCHAR DEFAULT 'Consumer' NOT NULL,
-                    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-                    modified_at TIMESTAMP DEFAULT NOW() NOT NULL,
-                    timezone VARCHAR DEFAULT 'UTC' NOT NULL
-                )
-            """)
-            # TODO: fix this to change core migrations to managed=True soon
-            # and you can remove this fixture
             connection.commit()
 
 
